55f66a99c3f9aa002018f7ce6af2b5c341ea9962
djkif <djkif1212@gmail.com>
2024-10-12 18:33:54 +0300

    app 2

--- lib/login.dart
+++ lib/login.dart
@@ -12,41 +12,42 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-          child: ListView(
-        padding: const EdgeInsets.symmetric(horizontal: 24),
-        children: <Widget>[
-          const SizedBox(height: 80.0),
-          Column(
-            children: <Widget>[
-              Image.asset('assets/diamond.png'),
-              const SizedBox(height: 16.0),
-              const Text('SHRINE'),
-            ],
-          ),
-          const SizedBox(height: 120.0),
-          //
-          TextField(
+        child: ListView(
+          padding: const EdgeInsets.symmetric(horizontal: 24),
+          children: <Widget>[
+            const SizedBox(height: 80.0),
+            Column(
+              children: <Widget>[
+                Image.asset('assets/diamond.png'),
+                const SizedBox(height: 16.0),
+                const Text('SHRINE'),
+              ],
+            ),
+            const SizedBox(height: 120.0),
+            //
+            TextField(
               keyboardType: TextInputType.visiblePassword,
-            decoration: const InputDecoration(
-              border: OutlineInputBorder(),
-              filled: true,
-              labelText: 'User name',
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                filled: true,
+                labelText: 'User name',
+              ),
+              obscureText: false,
             ),
-            obscureText: false,
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          //
-          TextField(
-            decoration: const InputDecoration(
-              border: OutlineInputBorder(),
-              filled: true,
-              labelText: 'Password',
+            const SizedBox(
+              height: 20,
             ),
-            obscureText: true, // маскує текст (для паролів)
-          ),
-        ],
-      )),
-    );
-  }//1
+            //
+            TextField(
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                filled: true,
+                labelText: 'Password',
+              ),
+              obscureText: true, // маскує текст (для паролів)
+            ),
+          ],
+        ),
+      ),
+    );//1
+  }
