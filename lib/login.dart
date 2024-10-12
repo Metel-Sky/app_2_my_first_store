801c6d3072ce76c8c140ed301c0482e4660d9b10
djkif <djkif1212@gmail.com>
2024-10-12 18:32:07 +0300

    app 2

--- lib/login.dart
+++ lib/login.dart
@@ -7,11 +7,12 @@ class LoginPage extends StatefulWidget {
   _LoginPageState createState() => _LoginPageState();
 }
 
-class _LoginPageState extends State<LoginPage>{
+class _LoginPageState extends State<LoginPage> {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(child: ListView(
+      body: SafeArea(
+          child: ListView(
         padding: const EdgeInsets.symmetric(horizontal: 24),
         children: <Widget>[
           const SizedBox(height: 80.0),
@@ -23,10 +24,30 @@ class _LoginPageState extends State<LoginPage>{
             ],
           ),
           const SizedBox(height: 120.0),
+          //
+          TextField(
+              keyboardType: TextInputType.visiblePassword,
+            decoration: const InputDecoration(
+              border: OutlineInputBorder(),
+              filled: true,
+              labelText: 'User name',
+            ),
+            obscureText: false,
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          //
+          TextField(
+            decoration: const InputDecoration(
+              border: OutlineInputBorder(),
+              filled: true,
+              labelText: 'Password',
+            ),
+            obscureText: true, // маскує текст (для паролів)
+          ),
         ],
       )),
     );
   }
-
-
 }
